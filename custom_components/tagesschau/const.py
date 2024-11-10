38fdc697a3ea3c9bc6f2923f8fb5ece96552229d
timniklas <accounts@tnkoch.de>
2024-11-10 17:29:29 +0100

    Update const.py

--- custom_components/tagesschau/const.py
+++ custom_components/tagesschau/const.py
@@ -1,1 +1,20 @@
 DOMAIN = "tagesschau"
+
+REGIONS = {
+    1: 'Baden-Württemberg',
+    2: 'Bayern',
+    3: 'Berlin',
+    4: 'Brandenburg',
+    5: 'Bremen',
+    6: 'Hamburg',
+    7: 'Hessen',
+    8: 'Mecklenburg-Vorpommern',
+    9: 'Niedersachsen',
+    10: 'Nordrhein-Westfalen',
+    11: 'Rheinland-Pfalz',
+    12: 'Saarland',
+    13: 'Sachsen',
+    14: 'Sachsen-Anhalt',
+    15: 'Schleswig-Holstein',
+    16: 'Thüringen'
+}
